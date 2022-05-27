96a7822f9b8e89be4f26980d3ee56e173bef0521
John <jonnyprof@gmail.com>
2022-05-27 14:00:40 +0200

    build(release): update to angular 13
    BREAKING CHANGE:
    * Replace tslint with eslint
    * Change protractor to cypress
    * Update third party libraries

--- projects/ngx-qrcode/src/lib/qrcode.component.spec.ts
+++ projects/ngx-qrcode/src/lib/qrcode.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { Component, ViewChild } from '@angular/core';
 
 import QRCode from 'qrcode';
@@ -15,7 +15,7 @@ describe('QrcodeComponent', () => {
   let toCanvasSpy: jasmine.Spy;
   const IMAGE_SRC = 'imgSrc';
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [QrcodeComponent, TestHostComponent]
     })
